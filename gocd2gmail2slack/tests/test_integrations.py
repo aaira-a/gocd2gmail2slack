5ab985cb69d0020e6495373157f8677795485588
azam alias <azam.alias@nintex.com>
2016-03-06 09:08:44 +0800

    added initial integrations tests - gmail

--- gocd2gmail2slack/tests/test_integrations.py
+++ gocd2gmail2slack/tests/test_integrations.py
@@ -1,10 +1,30 @@
 
 import unittest
+from unittest.mock import patch
 
-from integrations import main
+from integrations import (
+    main,
+    WEBHOOK_URL,
+    GOCD_DASHBOARD_URL
+)
 
 
-class IntegrationsTests(unittest.TestCase):
+@patch('integrations.gm')
+class GmailIntegrationsTests(unittest.TestCase):
 
-    def test_scaffold(self):
-        pass
+    def test_gmail_service_is_initiated(self, mock):
+        main()
+        self.assertEqual(1, mock.get_service.call_count)
+
+    def test_get_all_labels(self, mock):
+        main()
+        self.assertEqual(1, mock.get_labels.call_count)
+
+    def test_get_initial_emails_with_unread_label(self, mock):
+        main()
+        expected_argument = {'include_labels': ['UNREAD']}
+        self.assertIn(expected_argument, mock.get_messages.call_args)
+
+    def test_get_message_details_from_initial_emails(self, mock):
+        main()
+        self.assertEqual(1, mock.get_messages_details.call_count)
