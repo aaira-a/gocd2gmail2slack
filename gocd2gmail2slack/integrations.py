013819a0f45b56c6336b0f1abf698d3903d6a491
azam alias <azam.alias@nintex.com>
2016-03-06 22:33:41 +0800

    added more integrations tests, extracted functions from main for testability

--- gocd2gmail2slack/integrations.py
+++ gocd2gmail2slack/integrations.py
@@ -11,30 +11,38 @@ from cfg.config import (
 
 def main():
     try:
-        service = gm.get_service()
-        labels = gm.get_labels(service)
-        initial_messages = gm.get_messages(service, include_labels=['UNREAD'])
-        messages_details = gm.get_messages_details(service, initial_messages)
-
-        for message in messages_details:
-            subject = messages.get_subject(message)
-            if messages.is_gocd_pattern(subject):
-                gocd_details = messages.get_gocd_details(subject)
-                if slack.is_matching_send_rule(gocd_details):
-                    body = messages.get_body(message)
-                    changeset = messages.get_changeset_info(body)
-                    text = (slack
-                            .message_builder(gocd_details,
-                                             changeset,
-                                             GOCD_DASHBOARD_URL))
-                    slack.send_to_slack(text, WEBHOOK_URL)
-                    gm.add_label(service, messages.get_id(message),
-                                 'SENT_TO_SLACK', labels)
-
-            gm.remove_label(service, messages.get_id(message),
-                            'UNREAD', labels)
+        service, labels, messages_details = initialize()
+        process(service, labels, messages_details)
     except:
         pass
+
+
+def initialize():
+    service = gm.get_service()
+    labels = gm.get_labels(service)
+    initial_messages = gm.get_messages(service, include_labels=['UNREAD'])
+    messages_details = gm.get_messages_details(service, initial_messages)
+    return (service, labels, messages_details)
+
+
+def process(service, labels, messages_details):
+    for message in messages_details:
+        subject = messages.get_subject(message)
+        if messages.is_gocd_pattern(subject):
+            gocd_details = messages.get_gocd_details(subject)
+            if slack.is_matching_send_rule(gocd_details):
+                body = messages.get_body(message)
+                changeset = messages.get_changeset_info(body)
+                text = (slack
+                        .message_builder(gocd_details,
+                                         changeset,
+                                         GOCD_DASHBOARD_URL))
+                slack.send_to_slack(text, WEBHOOK_URL)
+                gm.add_label(service, messages.get_id(message),
+                             'SENT_TO_SLACK', labels)
+
+        gm.remove_label(service, messages.get_id(message),
+                        'UNREAD', labels)
 
 
 if __name__ == "__main__":
--- gocd2gmail2slack/tests/test_integrations.py
+++ gocd2gmail2slack/tests/test_integrations.py
@@ -1,30 +1,59 @@
 
+import ast
 import unittest
 from unittest.mock import patch
 
 from integrations import (
-    main,
-    WEBHOOK_URL,
-    GOCD_DASHBOARD_URL
+    initialize,
+    process,
 )
+
+from fixtures.gmail_labels import LABELS
+from fixtures.gmail_message_detail_1 import MESSAGE1
 
 
 @patch('integrations.gm')
-class GmailIntegrationsTests(unittest.TestCase):
+class GmailInitializerIntegrationsTests(unittest.TestCase):
 
     def test_gmail_service_is_initiated(self, mock):
-        main()
+        initialize()
         self.assertEqual(1, mock.get_service.call_count)
 
     def test_get_all_labels(self, mock):
-        main()
+        initialize()
         self.assertEqual(1, mock.get_labels.call_count)
 
     def test_get_initial_emails_with_unread_label(self, mock):
-        main()
+        initialize()
         expected_argument = {'include_labels': ['UNREAD']}
         self.assertIn(expected_argument, mock.get_messages.call_args)
 
     def test_get_message_details_from_initial_emails(self, mock):
-        main()
+        initialize()
         self.assertEqual(1, mock.get_messages_details.call_count)
+
+
+@patch('integrations.slack.send_to_slack')
+@patch('integrations.gm')
+class ProcessingIntegrationsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.messages_details = [MESSAGE1]
+        self.labels = LABELS
+
+    def test_unread_label_is_removed_after_processing(self,
+                                                      mock_gm, mock_slack):
+        process('service', LABELS, self.messages_details)
+        expected = 'UNREAD'
+        self.assertIn(expected, mock_gm.remove_label.call_args[0])
+
+    def test_sent_slack_label_is_added_after_processing(self,
+                                                        mock_gm, mock_slack):
+        process('service', LABELS, self.messages_details)
+        expected = 'SENT_TO_SLACK'
+        self.assertIn(expected, mock_gm.add_label.call_args[0])
+
+    def test_data_extraction_complete_and_sent_to_slack(self,
+                                                        mock_gm, mock_slack):
+        process('service', LABELS, self.messages_details)
+        self.assertEqual(1, mock_slack.call_count)
