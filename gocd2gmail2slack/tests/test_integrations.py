ddc7460413af8d766f650eab1790a1197631db7f
azam alias <azam.alias@nintex.com>
2016-03-07 21:38:52 +0800

    expanded integration test to verify data extraction and call to slack

--- gocd2gmail2slack/tests/test_integrations.py
+++ gocd2gmail2slack/tests/test_integrations.py
@@ -3,33 +3,34 @@ import ast
 import unittest
 from unittest.mock import patch
 
-from integrations import (
-    initialize,
-    process,
-)
+import integrations
 
 from fixtures.gmail_labels import LABELS
 from fixtures.gmail_message_detail_1 import MESSAGE1
+
+
+integrations.WEBHOOK_URL = 'http://hook.url'
+integrations.GOCD_DASHBOARD_URL = 'http://dash.url'
 
 
 @patch('integrations.gm')
 class GmailInitializerIntegrationsTests(unittest.TestCase):
 
     def test_gmail_service_is_initiated(self, mock):
-        initialize()
+        integrations.initialize()
         self.assertEqual(1, mock.get_service.call_count)
 
     def test_get_all_labels(self, mock):
-        initialize()
+        integrations.initialize()
         self.assertEqual(1, mock.get_labels.call_count)
 
     def test_get_initial_emails_with_unread_label(self, mock):
-        initialize()
+        integrations.initialize()
         expected_argument = {'include_labels': ['UNREAD']}
         self.assertIn(expected_argument, mock.get_messages.call_args)
 
     def test_get_message_details_from_initial_emails(self, mock):
-        initialize()
+        integrations.initialize()
         self.assertEqual(1, mock.get_messages_details.call_count)
 
 
@@ -43,17 +44,29 @@ class ProcessingIntegrationsTests(unittest.TestCase):
 
     def test_unread_label_is_removed_after_processing(self,
                                                       mock_gm, mock_slack):
-        process('service', LABELS, self.messages_details)
+        integrations.process('service', LABELS, self.messages_details)
         expected = 'UNREAD'
         self.assertIn(expected, mock_gm.remove_label.call_args[0])
 
     def test_sent_slack_label_is_added_after_processing(self,
                                                         mock_gm, mock_slack):
-        process('service', LABELS, self.messages_details)
+        integrations.process('service', LABELS, self.messages_details)
         expected = 'SENT_TO_SLACK'
         self.assertIn(expected, mock_gm.add_label.call_args[0])
 
     def test_data_extraction_complete_and_sent_to_slack(self,
                                                         mock_gm, mock_slack):
-        process('service', LABELS, self.messages_details)
+        integrations.process('service', LABELS, self.messages_details)
+
         self.assertEqual(1, mock_slack.call_count)
+        self.assertEqual('http://hook.url', mock_slack.call_args[0][1])
+
+        expected_body = {'username': 'go build status - passed',
+                         'icon_emoji': ':white_check_mark:',
+                         'text': '<http://dash.url/tab/pipeline/history/product.branch.CI|product.branch.CI>\n'
+                         'Changeset: '
+                         '<https://code.domain.com/tfs/products/_versionControl/changeset/01234|01234> '
+                         '- committer: cloud config changes'
+                         }
+        actual_body = ast.literal_eval(str(mock_slack.call_args[0][0]))
+        self.assertDictEqual(expected_body, actual_body)
